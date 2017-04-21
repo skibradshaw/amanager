35c7dc21533cedce8365e4691ea4263dc1210a3c
Tim Bradshaw <tim@alltrips.com>
2017-04-21 06:46:10 -0700

    Fix: Fixes missing span tag in Lease Show view

--- resources/views/leases/show.blade.php
+++ resources/views/leases/show.blade.php
@@ -67,7 +67,7 @@
                             <strong>Security Deposit:</strong> {{$lease->deposit_in_dollars}}<br>
                             <strong>Pet Deposit:</strong> {{$lease->pet_deposit_in_dollars}}<br>
                             <strong>Rent Balance:</strong> <span class="@if($lease->rentBalance()>0) label label-danger @endif">{{$lease->rent_balance_in_dollars}}</span><br>
-                            <strong>Deposit Balance:</strong> <span class="@if($lease->depositBalance()>0) label label-danger @endif">{{$lease->deposit_balance_in_dollars}}<br>
+                            <strong>Deposit Balance:</strong> <span class="@if($lease->depositBalance()>0) label label-danger @endif">{{$lease->deposit_balance_in_dollars}}</span><br>
                         </p>
                         <!-- <a href="{{route('leases.terminate',[$property,$apartment,$lease])}}" data-toggle="modal" data-target="#largeModal" class="btn btn-default btn-block">End Lease</a> -->
                     </div>
