11d5dd6f374340b6d38ef6e1566a7e7fcc1b7b05
Timothy Bradshaw <tbradshaw@Timothys-MacBook-Pro.local>
2017-04-23 10:43:15 -0700

    Adds Activity Monitoring to Models

--- app/BankAccount.php
+++ app/BankAccount.php
@@ -3,10 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+
 
 class BankAccount extends Model
 {
+	use LogsActivity;
+
 	protected $guarded = [];
+	protected $logOnlyDirty = true;
 
 	public function setNameAttribute($value)
 	{
--- app/Property.php
+++ app/Property.php
@@ -3,11 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+
 
 class Property extends Model
 {
+    use LogsActivity;
     //
     protected $guarded = [];
+    protected $logOnlyDirty = true;
 
     public function getUnpaidRentInDollarsAttribute()
     {
