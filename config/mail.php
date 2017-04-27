09a7dba952edda85010dd43b8dcca86bed2aefb2
Tim Bradshaw <tim@alltrips.com>
2017-04-26 17:27:08 -0700

    Feature: Adds email address and From name to Mail

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@amanager.co'),
+        'name' => env('MAIL_FROM_NAME', 'A-Manager'),
     ],
 
     /*
